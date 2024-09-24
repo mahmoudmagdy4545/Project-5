f252ef93ae69c61c4fd5375c3fbd94d794b755df
mahmoudmagdy4545 <mahmoudmagdy45456@gmail.com>
2024-09-24 03:26:26 +0300

    all project

---
+++ main.js
@@ -0,0 +1,14 @@
+let cnt=document.querySelector(".container");
+let img=document.querySelector(".img");
+// let image0=document.getElementById("0");
+// let image1=document.getElementById("1");
+// let image2=document.getElementById("2");
+// let image3=document.getElementById("3");
+// let image4=document.getElementById("4");
+
+function phone(src){
+    img.src=src;
+}
+function color(cl){
+    cnt.style.background=cl;
+}
